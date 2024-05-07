d4dee62012768d09fd4c3d4be555d908a7209f27
Edanriell <Edanriell@gmail.com>
2024-05-08 00:53:01 +0300

    added icon

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -5,7 +5,9 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	alias: {
 		"@features": fileURLToPath(new URL("./features", import.meta.url)),
-		"@pages": fileURLToPath(new URL("./pages", import.meta.url))
+		"@pages": fileURLToPath(new URL("./pages", import.meta.url)),
+		"@shared": fileURLToPath(new URL("./shared", import.meta.url)),
+		"@shared/ui": fileURLToPath(new URL("./shared/ui", import.meta.url))
 	},
 	modules: [],
 	css: ["@/app/styles/styles.scss"],
